d5ee8036f747e22c68b28a43c053168e46e55bc3
akshat-nathani <akshatiscoding@gmail.com>
2025-01-31 15:03:24 +0530

    table

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import college from '../assets/Tiet_Track_Mhostel.jpg'
 import styles from '../css/DashBoard.module.css'
 import Navbar from '../components/Navbar/Navbar.jsx'
-
+import Table from '../components/Table/Table'
 
 const DashBoard = () => {
   return (
     <>
-        <div classname="container">
-        <img src={college} alt="placeholder" className={styles.bg} />
-          <Navbar />
-        </div>
+      <div className={styles.bg}></div>
+      <Navbar />
+      <div className={styles.content}>
+        <Table className={styles.table} />
+      </div>
     </>
   )
 }
